ad0b8ca90b7ea6244edaa0ae941178bbd7902909
zackt113 <94644378+zackt113@users.noreply.github.com>
2022-01-28 13:09:41 -0800

    Update tests/testthat/test-plot_ci.R
    Co-authored-by: Margot Vore <39842078+voremargot@users.noreply.github.com>

--- tests/testthat/test-plot_ci.R
+++ tests/testthat/test-plot_ci.R
@@ -24,7 +24,7 @@ test_that("Check if error thrown for invalid y_axis input", {
 
 test_that("Check if error thrown for invalid save_result_to input", {
   expect_error(plot_ci(c(1, 2, 3), 1000,
-                       n=100, 
+                       n = 100, 
                        ci_level = 0.95, 
                        ci_random_seed = 123, 
                        save_result_to = 123))
