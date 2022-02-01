4b65ae3f210b89ca81c5b90bee9a5127e9550cc5
Zack Tang <zacktang113@gmail.com>
2022-02-01 02:21:55 -0800

    update tests

--- tests/testthat/test-plot_ci.R
+++ tests/testthat/test-plot_ci.R
@@ -2,16 +2,16 @@ test_that("Check if error thrown for invalid title input", {
   expect_error(plot_ci(c(1, 2, 3), 1000, n = 100, title = 123))
 })
 
-test_that("Check if error thrown for invalid x_axis input", {
-  expect_error(plot_ci(c(1, 2, 3), 1000, n = 100, x_axis = 123))
-})
-
 test_that("Check if error thrown for invalid y_axis input", {
   expect_error(plot_ci(c(1, 2, 3), 1000, n = 100, y_axis = 123))
 })
 
-test_that("Check if error thrown for invalid save_result_to input", {
-  expect_error(plot_ci(c(1, 2, 3), 1000, n = 100, save_result_to = 123))
+test_that("Check if error thrown for invalid path input", {
+  expect_error(plot_ci(c(1, 2, 3), 1000, n = 100, path = 123))
+})
+
+test_that("Check if error thrown for invalid path input value", {
+  expect_error(plot_ci(c(1, 2, 3), 1000, n = 100, path = "Users"))
 })
 
 test_that("Check the output is a ggplot object", {
